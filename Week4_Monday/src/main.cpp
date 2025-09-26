5d69738514b579b0c7f906391c96285a384b7f34
njpatter <34224135+njpatter@users.noreply.github.com>
2025-09-26 10:36:46 -0500

    Updated to where I stopped on Wednesday of Week 4

--- Week4_Monday/src/main.cpp
+++ Week4_Monday/src/main.cpp
@@ -1,29 +1,79 @@
-#include <Arduino.h>
+#include <Arduino.h> 
+#include <Wire.h> 
+#include <stdint.h> 
+#include <res/qw_fnt_5x7.h> 
+#include <math.h> 
 
-int buttonCounter = 0;
+volatile int buttonCounter = 0;
 bool prevPressed = false;
 int switchPin = 10;
 unsigned long debounceDelay = 50;
-unsigned long prevTime = 0;
+unsigned long doublePressTime = 500;
+unsigned long prevTime = 0; 
+
+enum PressType {
+  NoPress, // 0
+  SinglePress, //1 
+  DoublePress, //2
+  LongPress //3
+};
+
+enum MachineState {
+  OffState,
+  TwoAxis,
+  XAxis,
+  YAxis,
+  RawData,
+  StateLength
+};
+
+volatile PressType currentPress = NoPress;
+volatile MachineState currentState = OffState;
 
 void buttonPress() {
   unsigned long currentTime = millis();
   if (currentTime - prevTime > debounceDelay) {
-    buttonCounter++;
+    if (currentTime - prevTime < doublePressTime) {
+      currentPress = DoublePress;
+    } else {
+      currentPress = SinglePress;
+    }
     prevTime = currentTime;
+
   }
 }
 
 void setup() {
-  Serial.begin(9600);
-  delay(2000);
+  delay(1000);
+  Serial.begin(9600); 
+  while (!Serial) {
+    yield();
+  } 
   pinMode(switchPin, INPUT_PULLDOWN);
-  attachInterrupt(switchPin, buttonPress, CHANGE);
+  attachInterrupt(switchPin, buttonPress, RISING); 
 }
 
 void loop() {
-  Serial.print("Button tally: ");
-  Serial.println(buttonCounter); 
-  delay(1000);
-  
+  if (currentPress == DoublePress ) { //&& currentState != OffState
+    currentState = (MachineState)(((int)currentState + 1) % (int)StateLength);
+    currentState = (MachineState)max((int)currentState, 1);
+  }
+  if (currentPress != NoPress) {
+    Serial.print("Current State type: ");
+    Serial.println((int)currentState);
+    currentPress = NoPress;
+  }  
+
+  switch (currentState)
+  {
+  case OffState:
+    /* code */
+    break;
+  case TwoAxis: 
+
+    break;
+  default:
+    break;
+  }
+   
 }
