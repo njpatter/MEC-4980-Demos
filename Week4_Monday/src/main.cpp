28e5db70ae7ab3fbca2af3087969c16fa4737855
njpatter <34224135+njpatter@users.noreply.github.com>
2025-09-26 14:12:20 -0500

    Updated through Friday's class

--- Week4_Monday/src/main.cpp
+++ Week4_Monday/src/main.cpp
@@ -1,8 +1,15 @@
 #include <Arduino.h> 
 #include <Wire.h> 
-#include <stdint.h> 
-#include <res/qw_fnt_5x7.h> 
+#include <stdint.h>  
 #include <math.h> 
+// libraries for led screen and accel
+#include "SparkFun_BMI270_Arduino_Library.h"
+#include <SparkFun_Qwiic_OLED.h>
+
+// Create appropriate obj for led and accel
+
+QwiicMicroOLED myOLED; 
+BMI270 imu;
 
 volatile int buttonCounter = 0;
 bool prevPressed = false;
@@ -10,6 +17,14 @@ int switchPin = 10;
 unsigned long debounceDelay = 50;
 unsigned long doublePressTime = 500;
 unsigned long prevTime = 0; 
+
+float theta = 0.0;
+float psi = 0.0;
+float phi = 0.0;
+
+int arrLx[] = {0,  1, 1, 1,  2,  2, 2, 2, 2,  3,  3,  3, 3, 3, 3, 3};
+int arrLy[] = {0, -1, 0, 1, -2, -1, 0, 1, 2, -3, -2, -1, 0, 1, 2, 3};
+char pout[30];
 
 enum PressType {
   NoPress, // 0
@@ -29,6 +44,29 @@ enum MachineState {
 
 volatile PressType currentPress = NoPress;
 volatile MachineState currentState = OffState;
+
+// Triangle draw function
+void drawTriangle(int xOff, int yOff, int xDir, int yDir, bool swap) {
+  if (!swap) {
+    for (int i = 0; i < 16; i++) {
+      myOLED.pixel(xOff + xDir * arrLx[i], yOff + yDir * arrLy[i], 255);
+    }
+  } else {
+    for (int i = 0; i < 16; i++) {
+      myOLED.pixel(xOff + yDir * arrLy[i], yOff + xDir * arrLx[i], 255);
+    }
+  }
+  
+}
+
+float getXangle() {
+    return atan2(imu.data.accelX, sqrt(imu.data.accelY * imu.data.accelY + imu.data.accelZ * imu.data.accelZ)) * 180.0 / PI;
+}
+
+float getYangle() {
+    return atan2(imu.data.accelY, sqrt(imu.data.accelX * imu.data.accelX + imu.data.accelZ * imu.data.accelZ)) * 180.0 / PI;
+    //phi = atan2(sqrt(imu.data.accelY * imu.data.accelY + imu.data.accelX * imu.data.accelX), imu.data.accelZ);
+}
 
 void buttonPress() {
   unsigned long currentTime = millis();
@@ -51,6 +89,17 @@ void setup() {
   } 
   pinMode(switchPin, INPUT_PULLDOWN);
   attachInterrupt(switchPin, buttonPress, RISING); 
+
+  // Start accel
+  Wire.begin();
+  while (imu.beginI2C(0x68) != BMI2_OK) {
+    delay(1000);
+  }
+  // Start LED screen
+  while (!myOLED.begin()) {
+    delay(1000);
+  }
+  Serial.println("Everything started!!!!!");
 }
 
 void loop() {
@@ -64,16 +113,51 @@ void loop() {
     currentPress = NoPress;
   }  
 
+  // erase screen
+  myOLED.erase(); 
+  // get imu data
+  imu.getSensorData();
+  theta = getXangle();
+  psi = getYangle();
   switch (currentState)
   {
   case OffState:
-    /* code */
+    myOLED.text(5,5, "It worked!");
     break;
   case TwoAxis: 
+    if (theta > 0.0) {
+      drawTriangle(0, 23, 1, 1, false);
+    } else {
+      drawTriangle(63, 23, -1, 1, false);
+    }
+    if (psi > 0.0) {
+      drawTriangle(32, 0, 1, 1, true);
+    } else {
+      drawTriangle(32, 31, 1, -1, true);
+    }
+    
+    break;
+  case XAxis: 
+    // get accel data
+    // see if angle > some min
+    // draw triangle
+    break;
+  case YAxis: 
+
+    break;
+  case RawData: 
+    sprintf(pout, "ax: %.2f", imu.data.accelX);
+    myOLED.text(0,0, pout);
+    sprintf(pout, "ay: %.2f", imu.data.accelY);
+    myOLED.text(0,10, pout);
+    sprintf(pout, "az: %.2f", imu.data.accelZ);
+    myOLED.text(0,20, pout);
 
     break;
   default:
     break;
   }
+  // display
+  myOLED.display();
    
 }
